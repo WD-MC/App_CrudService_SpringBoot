4cc8c34ca9caede541ef0cab4100f05b50276ed4
Wagsong Djomegni <m.wagsong.djomegni@accenture.com>
2025-09-16 15:50:13 +0400

    test(): Ecriture des tests BDD pour la recuperation des equipements

--- src/test/java/com/example/registration/stepDefinitions/EquipmentSteps.java
+++ src/test/java/com/example/registration/stepDefinitions/EquipmentSteps.java
@@ -1,9 +1,9 @@
 package com.example.registration.stepDefinitions;
 
-
 import com.example.registration.employee.equipment.dto.EquipmentRequest;
 import com.example.registration.employee.equipment.repository.EquipmentRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import io.cucumber.java.Before;
 import io.cucumber.java.en.*;
 import org.junit.jupiter.api.Assertions;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +11,7 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
+import java.util.List;
 import java.util.Map;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
@@ -26,9 +27,23 @@ public class EquipmentSteps {
     private MvcResult response;
     private EquipmentRepository equipmentRepository;
 
+
+    @Autowired
+    public EquipmentSteps(EquipmentRepository equipmentRepository) {
+        this.equipmentRepository = equipmentRepository;
+    }
+
+    @Before
+    public void cleanDatabase() {
+        equipmentRepository.deleteAll();
+    }
+
+    /**
+     * Création d'un équipement
+     */
     @Given("un équipement avec les données suivantes:")
     public void un_equipement_avec_les_donnees_suivantes(io.cucumber.datatable.DataTable dataTable) {
-        Map<String, String> data = dataTable.asMaps().get(0);
+        Map<String, String> data = dataTable.asMaps().getFirst();
         this.equipmentRequest = new EquipmentRequest(
                 data.get("name"),
                 data.get("brand"),
@@ -57,6 +72,50 @@ public class EquipmentSteps {
     public void le_corps_de_la_reponse_doit_contenir(String expectedValue) throws Exception {
         String content = response.getResponse().getContentAsString();
         Assertions.assertTrue(content.contains(expectedValue), "La réponse ne contient pas: " + expectedValue);
+    }
+
+    /**
+     * Récupération de tous les équipements
+     */
+    @Given("Les équipements suivants existent :")
+    public void les_equipement_suivantes_exitent(io.cucumber.datatable.DataTable dataTable) throws Exception {
+        List<Map<String, String>> dataList = dataTable.asMaps();
+
+        for (Map<String, String> data : dataList) {
+            EquipmentRequest request = new EquipmentRequest(
+                    data.get("name"),
+                    data.get("brand"),
+                    Integer.parseInt(data.get("quantity")),
+                    data.get("employee"),
+                    data.get("status")
+            );
+
+            String json = objectMapper.writeValueAsString(request);
+            mockMvc.perform(post("/equipments")
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(json))
+                    .andReturn();
+        }
+    }
+
+    @When("J'envoie une requête GET via l’API")
+    public void envoi_une_requete_get_via_lapi() throws Exception {
+        this.response = mockMvc.perform(get("/equipments")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andReturn();
+    }
+
+    @Then("La reponse doit avoir le code {int}")
+    public void la_reponse_get_doit_avoir_le_code(int expectedStatus) {
+        int actualStatus = response.getResponse().getStatus();
+        Assertions.assertEquals(expectedStatus, actualStatus);
+    }
+
+    @And("Le nombre d'équipements retournés doit etre de {int}")
+    public void le_nombre_équipements_retournés_doit_etre(Integer expectedCount) throws Exception {
+        String content = response.getResponse().getContentAsString();
+        List<?> equipements = objectMapper.readValue(content, List.class);
+        Assertions.assertEquals(expectedCount.intValue(), equipements.size());
     }
 
 }
--- src/test/resources/features/equipment-management.feature
+++ src/test/resources/features/equipment-management.feature
@@ -7,3 +7,13 @@ Feature: API de Gestion des équipements
     When je crée l'équipement via l'API
     Then la réponse doit avoir le code 201
     And le corps de la réponse doit contenir "Laptop"
+
+  Scenario: Récupération de tous les équipements
+    Given Les équipements suivants existent :
+      | name    | brand | quantity | employee | status     |
+      | Laptop  | Dell  | 5        | Michele  | disponible |
+      | Monitor | Dell  | 6        | John     | attribue   |
+    When J'envoie une requête GET via l’API
+    Then La reponse doit avoir le code 200
+    And Le nombre d'équipements retournés doit etre de 2
+
